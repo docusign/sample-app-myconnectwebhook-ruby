dcbb0546612bf21c0f761f6179d4a732bea3d0ff
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2023-02-15 23:06:36 -0800

    authentication update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,44 +1,44 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require "docusign"
+# require "docusign"
 
-# Defaults to STDOUT: https://github.com/omniauth/omniauth#logging
-# Logs entries like:
-# (docusign) Setup endpoint detected, running now.
-# (docusign) Request phase initiated.
-# (docusign) Callback phase initiated.
-OmniAuth.config.logger = Rails.logger
+# # Defaults to STDOUT: https://github.com/omniauth/omniauth#logging
+# # Logs entries like:
+# # (docusign) Setup endpoint detected, running now.
+# # (docusign) Request phase initiated.
+# # (docusign) Callback phase initiated.
+# OmniAuth.config.logger = Rails.logger
 
-# https://github.com/omniauth/omniauth/wiki/FAQ#omniauthfailureendpoint-does-not-redirect-in-development-mode
-# otherwise a callback exception like the following will not get caught:
-# OmniAuth::Strategies::OAuth2::CallbackError (access_denied)
-# GET "/auth/docusign/callback?error=access_denied&
-#   error_message=The%20user%20did%20not%20consent%20to%20connecting%20the%20application.&state=
-# OmniAuth.config.failure_raise_out_environments = [] # defaults to: ['development']
+# # https://github.com/omniauth/omniauth/wiki/FAQ#omniauthfailureendpoint-does-not-redirect-in-development-mode
+# # otherwise a callback exception like the following will not get caught:
+# # OmniAuth::Strategies::OAuth2::CallbackError (access_denied)
+# # GET "/auth/docusign/callback?error=access_denied&
+# #   error_message=The%20user%20did%20not%20consent%20to%20connecting%20the%20application.&state=
+# # OmniAuth.config.failure_raise_out_environments = [] # defaults to: ['development']
 
-OmniAuth.config.allowed_request_methods = %i[post get]
+# OmniAuth.config.allowed_request_methods = %i[post get]
 
-config = Rails.application.config
-config.middleware.use OmniAuth::Builder do
-  # OAuth2 login request configuration
-  # OAuth2 login response callback message configuration is in OmniAuth::Strategies::Docusign in lib/docusign.rb
-  provider :docusign, config.integration_key, config.integration_secret, setup: lambda { |env|
-    strategy = env["omniauth.strategy"]
+# config = Rails.application.config
+# config.middleware.use OmniAuth::Builder do
+#   # OAuth2 login request configuration
+#   # OAuth2 login response callback message configuration is in OmniAuth::Strategies::Docusign in lib/docusign.rb
+#   provider :docusign, config.integration_key, config.integration_secret, setup: lambda { |env|
+#     strategy = env["omniauth.strategy"]
 
-    strategy.options[:client_options].site = config.app_url
-    strategy.options[:prompt] = "login"
-    strategy.options[:oauth_base_uri] = config.authorization_server
-    strategy.options[:target_account_id] = config.target_account_id
-    strategy.options[:allow_silent_authentication] = config.allow_silent_authentication
-    strategy.options[:client_options].authorize_url = "#{strategy.options[:oauth_base_uri]}/oauth/auth"
-    strategy.options[:client_options].user_info_url = "#{strategy.options[:oauth_base_uri]}/oauth/userinfo"
-    strategy.options[:client_options].token_url = "#{strategy.options[:oauth_base_uri]}/oauth/token"
-    strategy.options[:authorize_params].prompt = strategy.options.prompt unless strategy.options[:allow_silent_authentication]
-    # session = strategy.session
+#     strategy.options[:client_options].site = config.app_url
+#     strategy.options[:prompt] = "login"
+#     strategy.options[:oauth_base_uri] = config.authorization_server
+#     strategy.options[:target_account_id] = config.target_account_id
+#     strategy.options[:allow_silent_authentication] = config.allow_silent_authentication
+#     strategy.options[:client_options].authorize_url = "#{strategy.options[:oauth_base_uri]}/oauth/auth"
+#     strategy.options[:client_options].user_info_url = "#{strategy.options[:oauth_base_uri]}/oauth/userinfo"
+#     strategy.options[:client_options].token_url = "#{strategy.options[:oauth_base_uri]}/oauth/token"
+#     strategy.options[:authorize_params].prompt = strategy.options.prompt unless strategy.options[:allow_silent_authentication]
+#     # session = strategy.session
 
-    strategy.options[:authorize_params].scope = "signature extended signature openid click.manage click.send"
-    # "content spring_read spring_write spring_write group_read permission_read user_read user_write account_read
-    # domain_read identity_provider_read dtr.rooms.read dtr.rooms.write dtr.documents.read dtr.documents.write dtr.profile.read
-    # dtr.profile.write dtr.company.read dtr.company.write room_forms notary_write notary_read"
-  }
-end
+#     strategy.options[:authorize_params].scope = "signature extended signature openid click.manage click.send"
+#     # "content spring_read spring_write spring_write group_read permission_read user_read user_write account_read
+#     # domain_read identity_provider_read dtr.rooms.read dtr.rooms.write dtr.documents.read dtr.documents.write dtr.profile.read
+#     # dtr.profile.write dtr.company.read dtr.company.write room_forms notary_write notary_read"
+#   }
+# end
